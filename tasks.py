93a9e71716309faf2b44fe38c1e3738dd1dc675f
shashidhara <ssdrnr9999@gmail.com>
2026-05-21 18:14:16 +0530

    task object

---
+++ tasks.py
@@ -0,0 +1,13 @@
+
+
+class tasks:
+    id:int
+    task:str
+    status:bool
+    tag:int
+    def __init__(self,id,task,status,tag):
+        self.id=id
+        self.task=task
+        self.status=status
+        self.tag=tag
+
